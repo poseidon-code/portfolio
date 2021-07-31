e5902e0d87b286d4d04d2c24f0260074b10a1a74
poseidon-code <pritamhalder05062000@gmail.com>
2021-07-31 13:25:13 +0530

    fix: nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,26 +5,30 @@ const transporter = Nodemailer.createTransport({
     port: 465,
     host: 'smtp.gmail.com',
     auth: {
-        user: 'pritamhalder.portfolio@gmail.com',
+        user: process.env.EFROM,
         pass: process.env.PASSWORD,
     },
 });
 
 export default (req, res) => {
-    if (req.method == 'POST') {
-        const mail = {
-            from: 'pritamhalder.portfolio@gmail.com',
-            to: 'pritamhalder05062000@gmail.com',
-            subject: `${req.body.subject} - ${req.body.name}`,
-            text: `Name: ${req.body.name} \nEmail: ${req.body.email} \n${req.body.message}`,
-        };
+    return new Promise((resolve) => {
+        if (req.method == 'POST') {
+            const mail = {
+                from: process.env.EFROM,
+                to: process.env.ETO,
+                subject: `${req.body.subject} - ${req.body.name}`,
+                text: `Name: ${req.body.name} \nEmail: ${req.body.email} \n${req.body.message}`,
+            };
 
-        transporter.sendMail(mail, (err) => {
-            if (err) {
-                res.status(500).send();
-            } else {
-                res.status(200).send();
-            }
-        });
-    }
+            transporter.sendMail(mail, (err) => {
+                if (err) {
+                    res.status(500).send();
+                    return resolve();
+                } else {
+                    res.status(200).send();
+                    return resolve();
+                }
+            });
+        }
+    });
 };
