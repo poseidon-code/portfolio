2710fecf162409e4eb8c1162789a9741374f0ce7
poseidon-code <pritamhalder05062000@gmail.com>
2021-08-01 23:07:33 +0530

    update: improved SEO

--- components/UI/Layout.jsx
+++ components/UI/Layout.jsx
@@ -1,5 +1,26 @@
+import Head from 'next/head';
+
 const Layout = (props) => {
-    return <main className='container'>{props.children}</main>;
+    return (
+        <>
+            <Head>
+                <title>Pritam Halder</title>
+                <meta name='title' content='Pritam Halder Portfolio' />
+                <meta name='robots' content='index, follow' />
+                <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
+                <meta
+                    name='description'
+                    content="I'm a Kolkata based software engineer who specializes in building and designing full stack products leading projects from research to implementation."
+                />
+                <meta name='author' content='poseidon-code' />
+                <meta
+                    name='keywords'
+                    content='portfolio, pritam halder, pritam, developer, pritam halder portfolio designer portfolio, creative portfolio'
+                />
+            </Head>
+            <main className='container'>{props.children}</main>
+        </>
+    );
 };
 
 export default Layout;
