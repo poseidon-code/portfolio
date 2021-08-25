ec218add0f4ad73f2f85802d77cb1ae54058ea49
poseidon-code <pritamhalder05062000@gmail.com>
2021-08-25 23:47:35 +0530

    create: /projects

---
+++ pages/projects.jsx
@@ -0,0 +1,39 @@
+import styles from '../styles/Projects.module.scss';
+
+const Projects = () => {
+    return (
+        <>
+            <section className={styles.header}>
+                <pre style={{ fontSize: '.3rem', fontFamily: 'monospace' }}>
+                    {`
+   ▐▀▄       ▄▀▌   ▄▄▄▄▄▄▄
+   ▌▒▒▀▄▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄
+  ▐▒▒▒▒▀▒▀▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄
+  ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▀▄
+▀█▒▒▒█▌▒▒█▒▒▐█▒▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌
+▀▌▒▒▒▒▒▒▀▒▀▒▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐   ▄▄
+▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌▄█▒█
+▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒█▀
+▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀
+▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌
+ ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+ ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌
+  ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+  ▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌
+    ▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀
+`}
+                </pre>
+                <h2>"You’re purrfect"</h2>
+                <h1>Projects</h1>
+                <h2>Imagination is more important than knowledge.</h2>
+            </section>
+
+            <section className={styles.footer}>
+                <p>Made with ❤ using NextJs by yours truly.</p>
+                <span>&copy; Pritam Halder 2021</span>
+            </section>
+        </>
+    );
+};
+
+export default Projects;
---
+++ utility/ProjectsData.js
@@ -0,0 +1,7 @@
+import formatCount from './formatCount';
+
+export default async () => {
+    const data = {};
+
+    return data;
+};
