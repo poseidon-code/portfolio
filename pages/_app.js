bbb349e64b143fd8883a98acdde6d5904cc508cc
Pritam Halder <pritamhalder05062000@gmail.com>
2021-09-13 15:37:07 +0530

    add: graphql support

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo';
+
 import '../styles/globals.scss';
 import Layout from '../components/UI/Layout';
 
 const App = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ApolloProvider client={client}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider>
     );
 };
 
