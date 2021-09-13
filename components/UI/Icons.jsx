b81e0464b7bcb5f45eaf0723168ee96a4a5fbf79
Pritam Halder <pritamhalder05062000@gmail.com>
2021-09-13 16:18:22 +0530

    add: Folder icon

--- components/UI/Icons.jsx
+++ components/UI/Icons.jsx
@@ -521,3 +521,14 @@ export const Logo = () => (
         </g>
     </svg>
 );
+
+export const Folder = () => (
+    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+        <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z'
+        />
+    </svg>
+);
