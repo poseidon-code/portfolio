84b0ab0aaca679b08bc3f7570a11b7bead8502a5
Pritam Halder <pritamhalder05062000@gmail.com>
2021-09-13 15:37:44 +0530

    create: data fetch for /projects

--- utility/ProjectsData.js
+++ utility/ProjectsData.js
@@ -1,7 +1,45 @@
 import formatCount from './formatCount';
+import { gql } from '@apollo/client';
+import client from '../apollo';
 
-export default async () => {
-    const data = {};
+const PROJECTDATA = gql`
+    query GetData {
+        projects {
+            id
+            name
+            description
+            technologies
+            links
+        }
+        openSourceContributions {
+            id
+            name
+            description
+            technologies
+            links
+        }
+    }
+`;
 
-    return data;
+const get = async () => {
+    const { data } = await client.query({
+        query: PROJECTDATA,
+    });
+
+    return {
+        projectdata: data,
+        // githubdata: ,
+        // githubstats: ,
+    };
+};
+
+export const SYMBOLS = ['出', '長', '前', '道', '意', '和', '定', '思', '民', '明', '高', '雨', '気', '森', '東', '青', '海', '宮', '命', '聖'];
+
+export const projectData = async () => {
+    const { projectdata } = await get();
+
+    return {
+        projects: projectdata.projects,
+        opensourcecontributions: projectdata.openSourceContributions,
+    };
 };
