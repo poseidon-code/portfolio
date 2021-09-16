51caabf9205cc42b0ea113f2d002f076fbea47f4
Pritam Halder <pritamhalder05062000@gmail.com>
2021-09-16 13:52:00 +0530

    remove: graphql, @apollo/client

--- apollo.js
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export default new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache(),
-});
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
-import { ApolloProvider } from '@apollo/client';
-import client from '../apollo';
-
 import '../styles/globals.scss';
 import Layout from '../components/UI/Layout';
 
 const App = ({ Component, pageProps }) => {
     return (
-        <ApolloProvider client={client}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ApolloProvider>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     );
 };
 
