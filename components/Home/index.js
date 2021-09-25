6269d16f8eaef601e221b0a1bf779a5e08a3020f
poseidon-code <pritamhalder05062000@gmail.com>
2021-09-26 00:00:37 +0530

    add: import/exports for page components

---
+++ components/Home/index.js
@@ -0,0 +1,6 @@
+import Project from './Project';
+import Skill from './Skill';
+import Stat from './Stat';
+import Technology from './Technology';
+
+export { Project, Skill, Stat, Technology };
---
+++ components/UI/index.js
@@ -0,0 +1,5 @@
+import Button from './Button';
+import SectionButton from './SectionButton';
+import Layout from './Layout';
+
+export { Button, SectionButton, Layout };
