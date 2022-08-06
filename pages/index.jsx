387a7e74f9f934773d4b2bd87ed13632a2bf4475
poseidon-code <61552390+poseidon-code@users.noreply.github.com>
2022-08-06 22:25:36 +0530

    refactor: remove contact API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect, Fragment } from 'react';
+import { useEffect, Fragment } from 'react';
 import axios from 'axios';
 
 import styles from '../styles/Home.module.scss';
@@ -16,6 +16,8 @@ import {
     Instagram,
     Linkedin,
     Resume,
+    Email,
+    Telegram,
 } from '../components/UI/Icons';
 
 export const getStaticProps = async () => {
@@ -29,42 +31,9 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ stats: { hours, visitors, repos, frameworks } }) => {
-    const nameRef = useRef();
-    const emailRef = useRef();
-    const messageRef = useRef();
-    const subjectRef = useRef();
-
-    const [sending, setSending] = useState(false);
-
     useEffect(() => {
         axios.get('https://api.countapi.xyz/hit/pritamh.netlify.app/');
     }, []);
-
-    const resetForm = () => {
-        nameRef.current.value = null;
-        emailRef.current.value = null;
-        messageRef.current.value = null;
-        subjectRef.current.value = null;
-    };
-
-    const submitHandler = e => {
-        e.preventDefault();
-        setSending(true);
-
-        const data = {
-            name: nameRef.current.value,
-            email: emailRef.current.value,
-            subject: subjectRef.current.value,
-            message: messageRef.current.value,
-        };
-
-        axios.post('/api/contact', data).then(res => {
-            if (res.status == 200) {
-                resetForm();
-            }
-            setSending(false);
-        });
-    };
 
     return (
         <>
@@ -74,7 +43,6 @@ const Home = ({ stats: { hours, visitors, repos, frameworks } }) => {
                 <h1>Pritam Halder</h1>
                 <h2>I build things for the web.</h2>
             </section>
-
             <div className={styles.introduction}>
                 <section className={styles.cta}>
                     <p>{homeDataStore.introduction}</p>
@@ -113,8 +81,8 @@ const Home = ({ stats: { hours, visitors, repos, frameworks } }) => {
                 <div className={styles.skills_head}>
                     <h1>Technical Skills</h1>
                     <h2>
-                        Modern technologies & abundant freely accessible resources helped me to upgrade from traditional
-                        methods to new approaches along with my programming confidence.
+                        Modern technologies & abundant freely accessible learning resources helped me to upgrade from
+                        traditional methods to new approaches along with my programming confidence.
                     </h2>
                 </div>
                 {/* TODO : update @for-thorugh count in Home.module.scss (.skills.skill-body.skill) */}
@@ -132,8 +100,8 @@ const Home = ({ stats: { hours, visitors, repos, frameworks } }) => {
                 <div className={styles.technologies_head}>
                     <h1>Technologies Tackled</h1>
                     <h2>
-                        Experienced with most of the important coding frameworks and proficient with with both
-                        javascript based backend & frontend development.
+                        Experienced with most of the important coding frameworks and proficient in both javascript based
+                        backend & frontend development.
                     </h2>
                 </div>
                 {/* TODO : update @for-thorugh count in Home.module.scss (.technologies.technologies-body.technology) */}
@@ -179,52 +147,24 @@ const Home = ({ stats: { hours, visitors, repos, frameworks } }) => {
                     <h2>Sometimes later becomes never...</h2>
                 </div>
                 <div className={styles.contact_links}>
-                    <div>
-                        <a href='https://facebook.com/pritamhalder0506' target='_blank' rel='noopener noreferrer'>
-                            <Facebook />
-                        </a>
-                        <a href='https://instagram.com/pritam.poseidon' target='_blank' rel='noopener noreferrer'>
-                            <Instagram />
-                        </a>
-                        <a
-                            href='https://linkedin.com/in/pritam-halder-8306741b3'
-                            target='_blank'
-                            rel='noopener noreferrer'>
-                            <Linkedin />
-                        </a>
-                        <a href='https://github.com/poseidon-code' target='_blank' rel='noopener noreferrer'>
-                            <Github />
-                        </a>
-                    </div>
-                    <form className={styles.contact_body} method='POST' onSubmit={submitHandler}>
-                        <input disabled={sending} type='text' ref={nameRef} name='name' placeholder='Name' required />
-                        <input
-                            disabled={sending}
-                            type='email'
-                            ref={emailRef}
-                            name='email'
-                            placeholder='Email'
-                            required
-                        />
-                        <input
-                            disabled={sending}
-                            type='text'
-                            ref={subjectRef}
-                            name='subject'
-                            placeholder='Subject'
-                            required
-                        />
-                        <textarea
-                            disabled={sending}
-                            placeholder='Your Message'
-                            ref={messageRef}
-                            name='message'
-                            rows='6'
-                            required></textarea>
-                        <Button disabled={sending} type='submit' style={{ '--hue': 0 }}>
-                            {sending ? 'Sending...' : 'Send'}
-                        </Button>
-                    </form>
+                    <a href='mailto:pritamhalder.portfolio@gmail.com' target='_blank' rel='noopener noreferrer'>
+                        <Email /> Drop a mail...
+                    </a>
+                    <a href='t.me/pritam_poseidon' target='_blank' rel='noopener noreferrer'>
+                        <Telegram /> Let's have a chat...
+                    </a>
+                    <a href='https://facebook.com/pritamhalder0506' target='_blank' rel='noopener noreferrer'>
+                        <Facebook />
+                    </a>
+                    <a href='https://instagram.com/pritam.poseidon' target='_blank' rel='noopener noreferrer'>
+                        <Instagram />
+                    </a>
+                    <a href='https://linkedin.com/in/pritamhalder0506' target='_blank' rel='noopener noreferrer'>
+                        <Linkedin />
+                    </a>
+                    <a href='https://github.com/poseidon-code' target='_blank' rel='noopener noreferrer'>
+                        <Github />
+                    </a>
                 </div>
             </section>
 
