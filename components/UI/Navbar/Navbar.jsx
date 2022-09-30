56cd9faad3d880c6eccb85d0bb9b5fd70a0f3cc3
poseidon-code <61552390+poseidon-code@users.noreply.github.com>
2022-09-30 10:00:00 +0530

    update: tab indices for nav links

--- components/UI/Navbar/Navbar.jsx
+++ components/UI/Navbar/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = props => {
         <nav className={styles.navbar} style={{ '--hue': props.color }}>
             <div className={styles.links}>
                 <Link href='/projects'>
-                    <a>
+                    <a tabIndex={1}>
                         <Projects />
                         <span>Projects</span>
                     </a>
                 </Link>
                 <Link href='/blog'>
-                    <a>
+                    <a tabIndex={2}>
                         <Blog />
                         <span>Blog</span>
                     </a>
@@ -21,20 +21,20 @@ const Navbar = props => {
             </div>
             <div className={styles.logo}>
                 <Link href='/'>
-                    <a>
+                    <a tabIndex={3}>
                         <Logo />
                     </a>
                 </Link>
             </div>
             <div className={styles.links}>
                 <Link href='/about'>
-                    <a>
+                    <a tabIndex={4}>
                         <About />
                         <span>About</span>
                     </a>
                 </Link>
                 <Link href='/#contact'>
-                    <a>
+                    <a tabIndex={5}>
                         <Contact />
                         <span>Contact</span>
                     </a>
