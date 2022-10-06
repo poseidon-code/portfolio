101bbe28015ab98ad8d30332586be9e526275318
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-10-06 10:00:00 +0530

    refactor: decouple SectionButton

--- components/UI/Button/SectionButton.jsx
+++ components/UI/Button/SectionButton.jsx
@@ -1,10 +1,25 @@
-import Button from './Button';
+import Link from 'next/link';
+import styles from './SectionButton.module.scss';
 
-const SectionButton = ({ children, text, ...rest }) => {
+const SectionButton = props => {
     return (
-        <div>
-            <h1>{text}</h1>
-            <Button {...rest}>{children}</Button>
+        <div className={styles.SectionButton} style={props.style}>
+            <h1>{props.text}</h1>
+            {props.link ? (
+                <Link href={props.href}>
+                    <a tabIndex={-1}>
+                        {/* prettier-ignore */}
+                        <style jsx>{`a { background-image: url('${props.background}'); }`}</style>
+                        <span>{props.children} &rarr;</span>
+                    </a>
+                </Link>
+            ) : (
+                <a tabIndex={-1} href={props.href} target={props.target} rel={props.rel}>
+                    {/* prettier-ignore */}
+                    <style jsx>{`a { background-image: url('${props.background}'); }`}</style>
+                    <span>{props.children} &rarr;</span>
+                </a>
+            )}
         </div>
     );
 };
