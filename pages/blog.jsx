8b5f8bc914658fe117e77a5b9e05f2b0c33ac62a
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-10-30 22:57:29 +0530

    fix: construction page (/blog) styles

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -5,11 +5,13 @@ import { Working } from '@icons';
 const Construction = () => {
     return (
         <section className='construction'>
-            <Working />
-            <h1>Opps ! This page is under construction.</h1>
-            <span>
-                &larr;&nbsp;<Link href='/'>Go Back</Link>
-            </span>
+            <div>
+                <Working />
+                <h1>Opps ! This page is under construction.</h1>
+                <span>
+                    &larr;&nbsp;<Link href='/'>Go Back</Link>
+                </span>
+            </div>
         </section>
     );
 };
