8c038946bbce1328c88ff7a6ce55882076994e5c
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-06-06 10:00:00 +0530

    refactor: disable all countapi calls

--- utility/HomeData.js
+++ utility/HomeData.js
@@ -28,58 +28,59 @@ const get_hours = async () => {
 // uses: Base URL, endpoints & routes of website to get times that endpoint was visited
 // requires: Base URL & endpoints
 const get_visitors = async () => {
-    // Base URLs
-    /**
-     * as one's domain name could change as many times as he wants,
-     * so to not loose any previous visit counts this array should include new domain as well as previous domains
-     * (unless the owner wishes to remove previous counts)
-     */
-    const SITES = ['pritamh.netlify.app'];
+    // // Base URLs
+    // /**
+    //  * as one's domain name could change as many times as he wants,
+    //  * so to not loose any previous visit counts this array should include new domain as well as previous domains
+    //  * (unless the owner wishes to remove previous counts)
+    //  */
+    // const SITES = ['pritamh.netlify.app'];
 
-    // Endpoints
-    /**
-     * it is required to have a base endpoint (i.e. '/') always
-     * as many endpoints could be included, avoiding any dynamic endpoints
-     */
-    const ROUTES = ['/', '/projects', '/about', '/blog'];
+    // // Endpoints
+    // /**
+    //  * it is required to have a base endpoint (i.e. '/') always
+    //  * as many endpoints could be included, avoiding any dynamic endpoints
+    //  */
+    // const ROUTES = ['/', '/projects', '/about', '/blog'];
 
-    // getcount() gets the number of visits to a URL passed
-    // params: URL (string)
-    // returns: a resolved promise with number of visits for a URL passed
-    const getcount = url => {
-        return new Promise((resolve, reject) => {
-            axios.get(url).then(res => {
-                // "value" is the number of counts that URL was visited
-                resolve(res.data.value);
-            });
-        });
-    };
+    // // getcount() gets the number of visits to a URL passed
+    // // params: URL (string)
+    // // returns: a resolved promise with number of visits for a URL passed
+    // const getcount = url => {
+    //     return new Promise((resolve, reject) => {
+    //         axios.get(url).then(res => {
+    //             // "value" is the number of counts that URL was visited
+    //             resolve(res.data.value);
+    //         });
+    //     });
+    // };
 
-    // getcounts ([Promises])
-    let getcounts = [];
-    /**
-     * looping over every possible endpoints for api.countapi.xyz/hit/{endpoints}
-     * and appending Promises (after calling getcounts()) for every endpoints
-     */
-    SITES.forEach(s => {
-        ROUTES.forEach(r => {
-            // appends Promise for every URL endpoints to getcounts[]
-            getcounts.push(getcount(`https://api.countapi.xyz/hit/${s}${r}`));
-        });
-    });
+    // // getcounts ([Promises])
+    // let getcounts = [];
 
-    // resolve all Promises inside getcount[] (API not working)
-    /**
-     * resolves all the Promises inside getcounts[] and saves the resolved data in place
-     * then from the array of resolved Promises (here, counts), looping and adding all numbers to get final count
-     * returns: summation of all counts
-     */
+    // /**
+    //  * looping over every possible endpoints for api.countapi.xyz/hit/{endpoints}
+    //  * and appending Promises (after calling getcounts()) for every endpoints
+    //  */
+    // SITES.forEach(s => {
+    //     ROUTES.forEach(r => {
+    //         // appends Promise for every URL endpoints to getcounts[]
+    //         getcounts.push(getcount(`https://api.countapi.xyz/hit/${s}${r}`));
+    //     });
+    // });
+
+    // // resolve all Promises inside getcount[] (API not working)
+    // /**
+    //  * resolves all the Promises inside getcounts[] and saves the resolved data in place
+    //  * then from the array of resolved Promises (here, counts), looping and adding all numbers to get final count
+    //  * returns: summation of all counts
+    //  */
     // const data = await Promise.all(getcounts).then(counts => {
     //     // sum of every number in the counts[]
     //     return counts.reduce((a, b) => a + b, 0);
     // });
 
-    return 7000;
+    return 8000;
 };
 
 // GET (REST) "Github Repos"
